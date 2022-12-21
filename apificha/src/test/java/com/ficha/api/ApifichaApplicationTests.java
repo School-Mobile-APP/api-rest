ff257869f6cef77d45ab7ab41f2297ca361b6e77
FelixPalacios117 <00055419@uca.edu.sv>
2022-12-21 14:38:59 -0600

    :bug: Project base added

---
+++ apificha/src/test/java/com/ficha/api/ApifichaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
