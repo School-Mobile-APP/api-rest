8a317b3569c4c9b449390d9a8c9c0d68f08e540c
Felix Guevara <00055419@uca.edu.sv>
2023-05-28 22:55:23 -0600

    changes

--- schoolapi/src/main/java/com/schoolapi/api/entities/Persona.java
+++ schoolapi/src/main/java/com/schoolapi/api/entities/Persona.java
@@ -1,5 +1,54 @@
 package com.schoolapi.api.entities;
 
-public class Persona {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="sg_personas",schema="centros_educativos")
+public class Persona implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long perPk;
+	@Column(name="per_primer_nombre")
+	private String perPrimerNombre;
+	@Column(name="per_segundo_nombre")
+	private String perSegundoNombre;
+	@Column (name="per_primer_apellido")
+	private String perPrimerApellido;
+	@Column (name="per_segundo_apellido")
+	private String perSegundoApellido;
+	@Column (name="per_email")
+	private String perEmail;
+	@OneToOne
+	@JoinColumn(name="per_etnia_fk")
+	private Etnia etnia;
+	@OneToOne
+	@JoinColumn(name="per_estado_civil_fk")
+	private EstadoCivil estadoCivil;
+	@OneToOne
+	@JoinColumn(name="per_sexo_fk")
+	private Sexo sexo;
+	@Column(name="per_cantidad_hijos")
+	private Integer perCantidadHijos;
+	@Column (name="per_dui")
+	private String perDui;
+	@OneToOne
+	@JoinColumn(name="per_nacionalidad_fk")
+	private Nacionalidad nacionalidad;
+	@OneToOne
+	@JoinColumn(name="per_direccion_fk")
+	private Direccion direccion;
+	@Column(name="per_partida_nacimiento_presenta")
+	private Boolean perPartidaNacimientoPresenta;
+	@OneToOne
+	@JoinColumn(name="per_tipo_trabajo_fk")
+	private TipoTrabajo tipoTrabajo;
+	
+	
 }
--- schoolapi/src/main/java/com/schoolapi/api/entities/Sede.java
+++ schoolapi/src/main/java/com/schoolapi/api/entities/Sede.java
@@ -9,18 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 @Table(name="sg_sedes",schema="centros_educativos")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Sede implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -33,4 +23,28 @@ public class Sede implements Serializable{
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name="sed_direccion_fk")
 	private Direccion direccion;
+	public long getSedPk() {
+		return sedPk;
+	}
+	public void setSedPk(long sedPk) {
+		this.sedPk = sedPk;
+	}
+	public String getSedNombre() {
+		return sedNombre;
+	}
+	public void setSedNombre(String sedNombre) {
+		this.sedNombre = sedNombre;
+	}
+	public String getSedCodigo() {
+		return sedCodigo;
+	}
+	public void setSedCodigo(String sedCodigo) {
+		this.sedCodigo = sedCodigo;
+	}
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
 }
